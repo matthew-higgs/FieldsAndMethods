7e108f33a4b885f01b89eaf6ee5368c0f4e0ca45
Chewbacca <higgimat000@warren.k12.in.us>
2024-10-28 13:09:23 -0400

    initial commit

--- src/App.java
+++ src/App.java
@@ -1,2 +1,17 @@
-package PACKAGE_NAME;public class App {
+public class App {
+    public static void main(String[] args) {
+
+        Library A = new Library();
+        A.name = "A";
+        A.addBooks(100);
+
+
+        // TODO: make B.  and do the same.
+
+        Library B = new Library();
+        B.name = "B";
+        B.addBooks(150);
+
+        System.out.println("Total books in all libraries: " + Library.getTotalBooks());
+    }
 }
